d483cfe94a810e52b7e99d1166954421a62d5e0e
Carolina Duarte Neves <carolinaduarteneves@gmail.com>
2026-06-23 15:45:52 -0300

    feat: add built files for password-generator

---
+++ front-end/section-01-introduction-to-frontend-and-asynchronous-javascript/day-01-development-environment/password-generator/vite.config.js
@@ -0,0 +1,5 @@
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  base: "/trybe-exercises/front-end/section-01-introduction-to-frontend-and-asynchronous-javascript/day-01-development-environment/password-generator/",
+});
